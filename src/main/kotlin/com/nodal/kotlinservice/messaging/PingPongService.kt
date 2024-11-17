fe642f2be744abe5afd8a6cb8afe2d837c7e880a
elixir-1 <katkoriavidhi@gmail.com>
2024-11-17 15:59:44 -0500

    Modify Rabbit Listener

--- src/main/kotlin/com/nodal/kotlinservice/messaging/PingPongService.kt
+++ src/main/kotlin/com/nodal/kotlinservice/messaging/PingPongService.kt
@@ -1,6 +1,8 @@
 package com.nodal.kotlinservice.messaging
 
 import io.micrometer.core.annotation.Timed
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.amqp.rabbit.annotation.Exchange
 import org.springframework.amqp.rabbit.annotation.Queue
 import org.springframework.amqp.rabbit.annotation.QueueBinding
@@ -14,17 +16,26 @@ class PingPongService(val rabbitTemplate: RabbitTemplate) {
     companion object {
         const val EXCHANGE_NAME = "com.kotlinservice"
         const val QUEUE_NAME = "PingPong"
+
+        val log: Logger = LoggerFactory.getLogger(PingPongService::class.java)
     }
 
-//    @RabbitListener(bindings = [
-//        QueueBinding(
-//            exchange = Exchange(value = EXCHANGE_NAME, durable = "false"),
-//            value = Queue(value = "${EXCHANGE_NAME}/${QUEUE_NAME}"),
-//            key = [QUEUE_NAME]
-//        )])
-    @RabbitListener(queues = [RabbitMQConfig.PING_QUEUE_NAME])
+    @RabbitListener(bindings = [
+        QueueBinding(
+            exchange = Exchange(value = EXCHANGE_NAME, durable = "false"),
+            value = Queue(value = "${EXCHANGE_NAME}/${QUEUE_NAME}"),
+            key = [QUEUE_NAME]
+        )])
     @Timed
     fun consumePingEvent(message: String) {
-        println("Received message: $message")
+//        println("Received message: $message")
+        log.info("Received message: $message")
+
+        Thread.sleep(10000)
+        rabbitTemplate.convertAndSend(
+            EXCHANGE_NAME,
+            PONG_QUEUE_NAME,
+            "pong"
+        )
     }
 }
