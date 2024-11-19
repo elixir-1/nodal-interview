ef966e8fd7c0cacc84b0ef27576d38c21c8ff817
elixir-1 <katkoriavidhi@gmail.com>
2024-11-19 04:42:19 -0500

    Add message publish and consume logic

--- src/main/kotlin/com/nodal/kotlinservice/messaging/PingPongService.kt
+++ src/main/kotlin/com/nodal/kotlinservice/messaging/PingPongService.kt
@@ -14,28 +14,47 @@ import org.springframework.stereotype.Service
 class PingPongService(val rabbitTemplate: RabbitTemplate) {
 
     companion object {
-        const val EXCHANGE_NAME = "com.kotlinservice"
-        const val QUEUE_NAME = "PingPong"
+        const val JAVA_SERVICE_EXCHANGE_NAME = "com.javaservice.exchange"
+        const val KOTLIN_SERVICE_EXCHANGE_NAME = "com.kotlinservice.exchange"
+        const val Q2_ROUTING_KEY = "queue2.routing.key"
+        const val Q1_ROUTING_KEY = "queue1.routing.key"
+
+        const val PING_MESSAGE = "ping"
+        const val PONG_MESSAGE = "pong"
+
+        const val SERVICE_NAME = "kotlin service"
 
         val log: Logger = LoggerFactory.getLogger(PingPongService::class.java)
     }
 
     @RabbitListener(bindings = [
         QueueBinding(
-            exchange = Exchange(value = EXCHANGE_NAME, durable = "false"),
-            value = Queue(value = "${EXCHANGE_NAME}/${QUEUE_NAME}"),
-            key = [QUEUE_NAME]
+            exchange = Exchange(value = KOTLIN_SERVICE_EXCHANGE_NAME, durable = "false"),
+            value = Queue(value = "$KOTLIN_SERVICE_EXCHANGE_NAME/$Q2_ROUTING_KEY"),
+            key = [Q2_ROUTING_KEY]
         )])
     @Timed
-    fun consumePingEvent(message: String) {
-//        println("Received message: $message")
-        log.info("Received message: $message")
+    fun consumeMessage(message: String) {
 
-        Thread.sleep(10000)
-        rabbitTemplate.convertAndSend(
-            EXCHANGE_NAME,
-            PONG_QUEUE_NAME,
-            "pong"
-        )
+        log.info("$SERVICE_NAME received message: $message")
+
+        if ((message == PING_MESSAGE)) {
+            publishMessage(PONG_MESSAGE)
+        } else {
+            try {
+                Thread.sleep(10000) // 10-second delay
+            } catch (e: InterruptedException) {
+                Thread.currentThread().interrupt()
+                System.err.println("Thread interrupted: " + e.message)
+            }
+
+            publishMessage(PING_MESSAGE)
+        }
+    }
+
+    fun publishMessage(message: String) {
+
+        rabbitTemplate.convertAndSend(JAVA_SERVICE_EXCHANGE_NAME, Q1_ROUTING_KEY, message)
+        log.info("$SERVICE_NAME sent message: $message")
     }
 }
